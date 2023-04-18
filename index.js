85b020439ef7d3498e8bff1d31857dbf61573361
dan <dancr.wley@gmail.com>
2023-04-18 09:42:30 -0400

    isPalindrome function is functional

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+  word = word.replace(/\s/g, '').toLowerCase();
+  reverseWord = word.split('').reverse().join('')
+ if (reverseWord === word) {
+  return true
+ } else { 
+  return false
+}}
 
 module.exports = isPalindrome;
